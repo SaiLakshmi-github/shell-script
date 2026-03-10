ed57dc7cec012e509ace4633983f7fe9d4165862
Sai Lakshmi <sailakshmi88@gmail.com>
2026-03-10 23:21:33 +0530

    recent

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2 #parameters or Arguments, variables can be declared outside the script
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hi $ERSON1, I am doing good. How are you?"
+echo "$PERSON1:: I am good, How is your work going on"
+echo "$PERSON2:: Currently I am looking to upgrade to DevOps"

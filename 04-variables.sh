df84e2176b0786fbc9c491a0a58cfdc90c4c18f3
Sai Lakshmi <sailakshmi88@gmail.com>
2026-03-10 23:30:11 +0530

    recent

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo " Enter the Username::"
+
+read -s Username #here user name is the variable
+
+echo "Enter the password:: "
+
+read -s Password
+
+echo " Username is : $Username: 
+Password is : $Password "

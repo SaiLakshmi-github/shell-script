778f00c8a6695a03b5f98371381b8aafb9651f8a
Sai Lakshmi <sailakshmi88@gmail.com>
2026-03-10 19:40:41 +0530

    modified the names

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PERSON1=Ramesh
-PERSON2=Suresh
+PERSON1=Sachin
+PERSON2=Rahul
 
 echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hi $ERSON1, I am doing good. How are you?"

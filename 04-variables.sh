ef16eb47f9b8f0593069e38937ab71d01f2ad7dd
Sai Lakshmi <sailakshmi88@gmail.com>
2026-03-10 23:31:47 +0530

    recent

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo " Enter the Username::"
+echo "Enter the Username::"
 
 read -s Username #here user name is the variable
 
-echo "Enter the password:: "
+echo "Enter the password::"
 
 read -s Password
 
-echo " Username is : $Username: 
-Password is : $Password "
+echo "Username is: $Username 
+Password is: $Password "

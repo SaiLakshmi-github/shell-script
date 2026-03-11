1f1ee7308ded22f46278d714014bf62033fc2a40
Sai Lakshmi <sailakshmi88@gmail.com>
2026-03-11 15:24:45 +0530

    recent

---
+++ 09-Installations.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if $(id -u -ne 0)
+then    
+    echo "Please run the script with superuser"
+else 
+    echo "You are superuser"
+    exit 1
+fi
+
+dnf install mysql -y
+exit 1 #Manually exit if there is an error
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation of mysql is Failed"
+exit 1
+else 
+    echo "Installation of mysql is successful"
+
+fi
+
+dnf install git  -y
+exit 1 #Manually exit if there is an error
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation of Git is Failed"
+else 
+    echo "Installation of GIT is successful"
+exit 1
+fi

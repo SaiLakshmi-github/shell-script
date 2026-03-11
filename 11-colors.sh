9cae947ee1c8a035880398b4a7fb243a74a43a96
Sai Lakshmi <sailakshmi88@gmail.com>
2026-03-11 23:57:30 +0530

    recent

--- 11-colors.sh
+++ 11-colors.sh
@@ -5,9 +5,9 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d '.' -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
-R=\e[31m
-G=\e[32m
-N=\e[0m
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VALIDATE() {
 if [ $1 -ne 0 ]

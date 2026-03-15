97de2a9a32616effb80c9bf20275322e6399ad36
Sai Lakshmi <sailakshmi88@gmail.com>
2026-03-16 00:17:05 +0530

    deleting old logs

---
+++ 18-diskusage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+DISKUSAGE=$(df -hT | grep xfs)
+DISKTHRESHOLD=6
+MESSAGE=""
+
+while IFS= read -r line
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    FOLDER=$(echo $line | awk -F " " '{print $6F}')
+    if [ $USAGE -gt $DISKTHRESHOLD ]
+    then
+     MESSAGE+="$FOLDER is more than $DISKTHRESHOLD, Current Usage: $USAGE \n"
+
+    fi
+done <<< $DISK_USAGE
+
+echo -e "Message: $MESSAGE"
+
+echo "$MESSAGE" | mail -s "Disk Usage Alert" sailakshmi88@gmail.com
+
+# echo "body" | mail -s "subject" to-address

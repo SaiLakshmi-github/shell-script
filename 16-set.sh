7b9714469aadefbcf9821ae602dc930ef968fabb
Sai Lakshmi <sailakshmi88@gmail.com>
2026-03-15 18:02:48 +0530

    calling other script from current script

---
+++ 16-set.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+failure() {
+    echo " failed at $1:$2"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=(id -u)
+if [ $USERID -ne 0 ]
+then
+   echo " Please install the package with superuser"
+   exit 1
+else
+   echo " You are Superuser"
+fi
+
+dnf install mysqlfff -y
+dnf install git 
+
